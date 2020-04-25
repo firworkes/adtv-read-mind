1a69409e60475a77d5f60b73cdf00439b7a5fd7f
343172268@qq.com <343172268@qq.com>
2020-04-26 01:23:20 +0800

    树节点新增互换位置

--- src/assets/js/mindCard.js
+++ src/assets/js/mindCard.js
@@ -1,5 +1,36 @@
-const carDragstart = (event) => {
-    console.log(event.x, event.y)
+var srcdiv = null;
+var temp = null;
+export const carDragstart = (event, divdom) => {  // 当开始拖动时
+    // event.preventDefault()
+    srcdiv = divdom;
+    temp = divdom.innerHTML;
+    // console.log(temp)
 }
 
-export default carDragstart;
+export const carOndragend = (event) => {  // 用户完成元素拖动后触发
+    // event.preventDefault()
+    // console.log("用户完成元素拖动")
+}
+
+export const carOndrop = (event, divdom) => {  // 用户释放鼠标时
+    event.preventDefault()
+    console.log(divdom)
+    if (srcdiv !== divdom) {
+        srcdiv.innerHTML = divdom.innerHTML;
+        divdom.innerHTML = temp;
+    }
+}
+
+export const carOndragenter = (event) => {  // 当被鼠标拖动的对象进入其容器范围内时触发此事件
+    // event.preventDefault()
+    // console.log(event.srcElement.textContent)
+    // srcElement
+}
+
+export const carOndragover = (event) => { // 当某被拖动的对象在另一对象容器范围内拖动时触发此事件
+    event.preventDefault()
+    // console.log("拖动的对象在另一对象容器范围内拖动时触发此事件")
+    // console.log(event.srcElement.textContent)
+}
+
+// export default carDragstart carOndragover;
--- src/views/TreeDemo.vue
+++ src/views/TreeDemo.vue
@@ -1,121 +1,267 @@
 <template>
   <div>
-      <div id="container"></div>
+    <div id="container"></div>
   </div>
 </template>
 
 <script>
-import G6 from '@antv/g6'
+import G6 from "@antv/g6";
+import {
+  carDragstart,
+  carOndragover,
+  carOndragend,
+  carOndragenter,
+  carOndrop
+} from "../assets/js/mindCard";
 export default {
-  mounted() {
-    fetch(
-      "https://gw.alipayobjects.com/os/antvdemo/assets/data/algorithm-category.json"
-    )
-      .then(res => res.json())
-      .then(data => {
-        G6.registerNode(
-            "dom-node",
-            {
-            draw: (cfg, group) => {
-                return group.addShape("dom", {
-                attrs: {
-                    width: cfg.size[0],
-                    height: cfg.size[1],
-                    // 传入 DOM 的 html，带有原生 onclick 事件
-                    html: `<div style="width: 50; height: 50;backgroundColor: red">3333</div>`
-                },
-                draggable: true
-                });
-            }
-            },
-            "single-node"
-        );
-
-        const width = document.getElementById("container").scrollWidth;
-        const height = document.getElementById("container").scrollHeight || 500;
-        const graph = new G6.TreeGraph({
-          renderer: "svg",
-          container: "container",
-          width,
-          height,
-          modes: {
-            default: [
-            //   {
-            //     type: "collapse-expand",
-            //     onChange: function onChange(item, collapsed) {  // 重新布局刷新视图前的事件
-            //       const data = item.get("model").data;
-            //       data.collapsed = collapsed;
-            //       return true;
-            //     }
-            //   },
-              "drag-canvas",
-              "zoom-canvas"
+  data() {
+    return {
+      data: {
+        id: "Modeling Methods",
+        children: [
+          {
+            id: "Classification",
+            children: [
+              {
+                id: "Logistic regression"
+              },
+              {
+                id: "Linear discriminant analysis"
+              },
+              {
+                id: "Rules"
+              },
+              {
+                id: "Decision trees"
+              },
+              {
+                id: "Naive Bayes"
+              },
+              {
+                id: "K nearest neighbor"
+              },
+              {
+                id: "Probabilistic neural network"
+              },
+              {
+                id: "Support vector machine",
+                children: [{ id: "新增节点111" }, { id: "新增节点222" }]
+              }
             ]
           },
-          defaultNode: {
-            // type: "dom-node",
-            size: 26,
-            anchorPoints: [
-              [0, 0.5],
-              [1, 0.5]
-            ],
-            style: {
-              fill: "#C6E5FF",
-              stroke: "#5B8FF9"
-            }
+          {
+            id: "Consensus",
+            children: [
+              {
+                id: "Models diversity",
+                children: [
+                  {
+                    id: "Different initializations"
+                  },
+                  {
+                    id: "Different parameter choices"
+                  },
+                  {
+                    id: "Different architectures"
+                  },
+                  {
+                    id: "Different modeling methods"
+                  },
+                  {
+                    id: "Different training sets"
+                  },
+                  {
+                    id: "Different feature sets"
+                  }
+                ]
+              },
+              {
+                id: "Methods",
+                children: [
+                  {
+                    id: "Classifier selection"
+                  },
+                  {
+                    id: "Classifier fusion"
+                  }
+                ]
+              },
+              {
+                id: "Common",
+                children: [
+                  {
+                    id: "Bagging"
+                  },
+                  {
+                    id: "Boosting"
+                  },
+                  {
+                    id: "AdaBoost"
+                  }
+                ]
+              }
+            ]
           },
-          defaultEdge: {
-            type: "cubic-horizontal",
-            style: {
-              stroke: "#A3B1BF"
-            }
+          {
+            id: "Regression",
+            children: [
+              {
+                id: "Multiple linear regression"
+              },
+              {
+                id: "Partial least squares"
+              },
+              {
+                id: "Multi-layer feedforward neural network"
+              },
+              {
+                id: "General regression neural network"
+              },
+              {
+                id: "Support vector regression"
+              }
+            ]
+          }
+        ]
+      }
+    };
+  },
+  mounted() {
+    this.showDom();
+  },
+  methods: {
+    showDom() {
+      // 拖拽事件绑定全局
+      window.carDragstart = carDragstart;
+      window.carOndragover = carOndragover;
+      window.carOndragend = carOndragend;
+      window.carOndragenter = carOndragenter;
+      window.carOndragenter = carOndragenter;
+      window.carOndrop = carOndrop;
+
+      G6.registerNode(
+        "dom-node",
+        {
+          draw: (cfg, group) => {
+            return group.addShape("dom", {
+              attrs: {
+                width: cfg.size[0],
+                height: cfg.size[1],
+                // 传入 DOM 的 html，带有原生 onclick 事件
+                html: `
+            <div 
+              draggable="true" 
+              ondrop="carOndrop(event, this)"
+              ondragstart="carDragstart(event, this)" 
+              ondragend="carOndragend(event)"
+              ondragenter="carOndragenter(event)"
+              ondragover="carOndragover(event)"
+              style="background-color: #fff; border: 2px solid #5B8FF9; border-radius: 5px; width: ${cfg
+                .size[0] - 5}px; height: ${cfg.size[1] - 5}px; display: flex;"
+            >
+              <div style="height: 100%; width: 33%; background-color: #CDDDFD">
+                <img alt="" style="line-height: 100%; padding-top: 6px; padding-left: 8px;" src="https://gw.alipayobjects.com/mdn/rms_f8c6a0/afts/img/A*Q_FQT6nwEC8AAAAAAAAAAABkARQnAQ" width="20" height="20" />  
+              </div>
+              <span style="margin:auto; padding:auto; color: #5B8FF9">${
+                cfg.label
+              }</span>
+            </div>
+          `
+              },
+              draggable: true
+            });
+          }
+        }
+        // "single-node"
+      );
+
+      const width = document.getElementById("container").scrollWidth || 900;
+      const height = document.getElementById("container").scrollHeight || 800;
+      const graph = new G6.TreeGraph({
+        renderer: "svg",
+        container: "container",
+        width,
+        height,
+        pixelRatio: 2,
+        modes: {
+          default: ["zoom-canvas", "drag-node"]
+        },
+        defaultNode: {
+          size: [200, 50],
+
+          // shape: "rect",
+          type: "dom-node",
+
+          style: {
+            fill: "#C6E5FF",
+            stroke: "#5B8FF9"
+          }
+        },
+        defaultEdge: {
+          type: "cubic-horizontal",
+          style: {
+            stroke: "#A3B1BF"
+          }
+        },
+        layout: {
+          type: "mindmap",
+          direction: "H",
+          getHeight: () => {
+            return 80;
           },
-          layout: {
-            type: "mindmap",
-            direction: "H",
-            getHeight: () => {  // 指定当前节点高度
-              return 16;
-            },
-            getWidth: () => {  // 指定当前节点宽度
-              return 16;
-            },
-            getVGap: () => {  // 指定当前节点的垂直间距
-              return 10;
-            },
-            getHGap: () => {  // 指定当前节点的水平间距
-              return 100;
-            },
-            getSide: () => {  // 指定当前节点在主题的左边 (left) 还是右边 (right)
-              return "right";
-            }
+          getWidth: () => {
+            return 160;
+          },
+          getVGap: () => {
+            return 10;
+          },
+          getHGap: () => {
+            return 100;
+          },
+          getSide: () => {
+            return "right";
           }
-        });
-
-        let centerX = 0;
-        // 所有节点的数据
-        graph.node(function(node) {
-          if (node.id === "Modeling Methods") {
-            centerX = node.x;
-          }
-
-          return {
-            label: node.id,
-            labelCfg: {
-              position:
-                node.children && node.children.length > 0
-                  ? "right"
-                  : node.x > centerX
-                  ? "right"
-                  : "left",
-              offset: 5
-            }
-          };
-        });
-
-        graph.data(data);
-        graph.render();
-        graph.fitView();
+        }
       });
+
+      let centerX = 0;
+      // 以下重点
+      graph.node(function(node) {
+        // depth 类似节点标识
+        if (node.depth == 0) {
+          // console.log(node);
+          return {
+            size: [100, 60],
+            style: {
+              fill: "red"
+              // stroke:''
+            },
+            label: node.id
+          };
+        }
+
+        if (node.depth == 1) {
+          // console.log(node);
+          return {
+            size: [100, 60],
+            style: {
+              fill: "blue"
+            },
+            label: node.id
+          };
+        }
+        return {
+          label: node.id, // 设置显示名称
+          labelCfg: {
+            // position: node.children && node.children.length > 0 ? 'left' : node.x > centerX ? 'right' : 'left', // 设置显示左右
+            offset: 5
+          }
+        };
+      });
+      graph.data(this.data);
+      graph.render();
+      graph.fitView();
+    }
   }
 };
 </script>
